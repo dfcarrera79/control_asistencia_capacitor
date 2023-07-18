74ab7315eff47cd33fd8bd9ae4a9685364ea1360
dfcarrera79 <44879886+dfcarrera79@users.noreply.github.com>
2023-07-18 13:50:14 -0500

    new commit

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,75 +1,17 @@
-// import { RouteRecordRaw } from 'vue-router';
-// import { useAuthStore } from '../stores/auth';
-// import multiguard from 'vue-router-multiguard';
-
-// const estaLogeado = (to: any, from: any, next: any) => {
-//   const appStore = useAuthStore();
-//   if (appStore.estaLogeado === false) {
-//     return next({
-//       path: '/login',
-//       query: { redirect: to.fullPath },
-//     });
-//   }
-//   return next();
-// };
-
-// const routes: RouteRecordRaw[] = [
-//   {
-//     path: '/',
-//     component: () => import('layouts/MainLayout.vue'),
-//     children: [
-//       {
-//         path: '',
-//         component: () => import('pages/IndexPage.vue'),
-//         beforeEnter: multiguard([estaLogeado]),
-//       },
-//     ],
-//   },
-
-//   {
-//     path: '/cambiar_clave/:ruc',
-//     component: () => import('layouts/LoginLayout.vue'),
-//     children: [
-//       { path: '', component: () => import('pages/CambiarClavePage.vue') },
-//     ],
-//   },
-//   {
-//     path: '/login',
-//     component: () => import('layouts/LoginLayout.vue'),
-//     children: [{ path: '', component: () => import('pages/LoginPage.vue') }],
-//   },
-
-//   // Always leave this as last one,
-//   // but you can also remove it
-//   {
-//     path: '/:catchAll(.*)*',
-//     component: () => import('pages/ErrorNotFound.vue'),
-//   },
-// ];
-
-// export default routes;
-
 import { RouteConfig } from 'vue-router';
-import multiguard from 'vue-router-multiguard';
-import { isAuthenticated } from 'guards';
 import { useAuthStore } from '../stores/auth';
+import multiguard from 'vue-router-multiguard';
 import { NavigationGuardNext, Route } from 'vue-router';
 
-const isAuthenticated = (
-  to: Route,
-  from: Route,
-  next: NavigationGuardNext
-): void => {
+const estaLogeado = (to: Route, from: Route, next: NavigationGuardNext) => {
   const appStore = useAuthStore();
-
-  if (!appStore.estaLogeado) {
-    next({
+  if (appStore.estaLogeado === false) {
+    return next({
       path: '/login',
       query: { redirect: to.fullPath },
     });
-  } else {
-    next();
   }
+  return next();
 };
 
 const routes: RouteConfig[] = [
@@ -80,10 +22,11 @@ const routes: RouteConfig[] = [
       {
         path: '',
         component: () => import('pages/IndexPage.vue'),
-        beforeEnter: multiguard([isAuthenticated]),
+        beforeEnter: multiguard([estaLogeado]),
       },
     ],
   },
+
   {
     path: '/cambiar_clave/:ruc',
     component: () => import('layouts/LoginLayout.vue'),
@@ -96,6 +39,7 @@ const routes: RouteConfig[] = [
     component: () => import('layouts/LoginLayout.vue'),
     children: [{ path: '', component: () => import('pages/LoginPage.vue') }],
   },
+
   // Always leave this as last one,
   // but you can also remove it
   {
